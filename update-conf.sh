caaadfdab67a17c02b1f44a0765b207dabb44d33
Fabien Alin <fabien.alin@gmail.com>
2018-08-31 11:32:51 +0200

    fixed conf

--- update-conf.sh
+++ update-conf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i.bak 's,https://dev.flyve.org/glpi,https://demo-api.flyve.org,g' $1
-cp $1 build/static/js
-mv $1.bk $1
+sed -i.bak 's,your_URL,https://demo-api.flyve.org,g' $1
+cp $1 build/ 
+mv $1.bak $1
