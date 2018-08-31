9e4884986585023381658c41e690149c26bc377f
Fabien Alin <fabien.alin@gmail.com>
2018-08-31 11:39:23 +0200

    added builded env

---
+++ build/config.js
@@ -0,0 +1,7 @@
+window.appConfig = {
+  glpiApiLink: "https://demo-api.flyve.org",
+  demoToken: "your_token",
+  pluginToken: "your_token",
+  appName: "MDM Dashboard",
+  bugsnag: "your_bugsnag_key",
+}
