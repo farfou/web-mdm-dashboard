15534863441799774ddedb97204333cd5aa4cf70
Fabien Alin <fabien.alin@gmail.com>
2018-08-31 12:07:27 +0200

    fixed conf injection

--- build/config.js
+++
@@ -1,7 +0,0 @@
-window.appConfig = {
-  glpiApiLink: "https://demo-api.flyve.org",
-  demoToken: "your_token",
-  pluginToken: "your_token",
-  appName: "MDM Dashboard",
-  bugsnag: "your_bugsnag_key",
-}
